4769be868bdcdbf65e154dec252e366ecbe62d10
manish sharma <manish.sharma23@gmail.com>
2019-12-30 18:34:01 +0530

    Test

---
+++ src/singletoneDP/MainClass.java
@@ -0,0 +1,11 @@
+package singletoneDP;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdsadasd ...");
+
+	}
+
+}
